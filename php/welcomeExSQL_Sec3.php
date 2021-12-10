24d3ff41b2d65febcf7d4812ec2bb8f44bae93d6
seokmin01 <79754613+seokmin01@users.noreply.github.com>
2021-12-10 19:34:29 +0900

    php test

---
+++ php/welcomeExSQL_Sec3.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your address is: <?php echo $_POST["address"]; ?><br>
+Your favorite store is: <?php echo $_POST["fav_store"]; ?><br>
+database insertion
+A record for name, address, and store has benn inserted.
+
+<h3><a>Home Site</a></h3>
+
+</body>
+</html>
